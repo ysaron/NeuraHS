83267176d959b59de0e8a3d649121bc976cf303d
ysaron <fr09sar0n@gmail.com>
2021-09-23 21:41:55 +0700

    Split base.html elements into separate files

--- neura_hs/neura_hs/settings.py
+++ neura_hs/neura_hs/settings.py
@@ -1,7 +1,4 @@
 from pathlib import Path
-from dotenv import load_dotenv, find_dotenv
-
-load_dotenv(find_dotenv(filename='.env.dev'))
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
